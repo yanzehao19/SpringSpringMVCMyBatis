042437f1eaffa99213dbd1e018e9d8a52ffd6aa4
zhyan5726 <zhyan5726@fiberhome.com>
2017-10-27 17:23:06 +0800

    [A]接口访问不了问题解决

--- src/main/java/com/cn/hnust/controller/UserController.java
+++ src/main/java/com/cn/hnust/controller/UserController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cn.hnust.pojo.User;
 import com.cn.hnust.service.IUserService;
@@ -15,13 +16,14 @@ import com.cn.hnust.service.IUserService;
 public class UserController {
 	@Resource
 	private IUserService userService;
-	
+	@ResponseBody 
 	@RequestMapping("/showUser")
 	public String toIndex(HttpServletRequest request,Model model){
 		  int userId = Integer.parseInt(request.getParameter("id"));  
 	        User user = this.userService.getUserById(userId);  
 	        model.addAttribute("user", user);  
-	        return "showUser";  
-	        //return user.getUserName();
+	        //return "showUser";  
+	        String name=user.getUserName();
+	        return name;
 	    }  
 	}  

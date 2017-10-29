c08e875ba32732bc670b5ab2f6ea21803e41ce3d
yanzehao19 <yanzehao19@163.com>
2017-10-29 20:55:42 +0800

    [A]

--- src/main/java/com/cn/hnust/controller/UserController.java
+++ src/main/java/com/cn/hnust/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.cn.hnust.pojo.User;
 import com.cn.hnust.service.IUserService;
 
 @Controller
